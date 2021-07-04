109c99fef20c0d0ef029162b7fbc65a7cb0ea0f0
Thaís <thais.calixto@mail.uft.edu.br>
2021-07-04 13:24:16 -0300

    overTheRoad upgraded
    Agora a função foi otimizada, sem ultrapassar a memória, fazendo um cálculo simples

--- lib/road.dart
+++ lib/road.dart
@@ -1,22 +1,4 @@
 int overTheRoad(int adress_number, int street_length) {
-  var right_house = <int>[];
-  var left_house = <int>[];
-
-  for (var house_number = 1;
-      house_number < street_length * 2;
-      house_number += 2) {
-    right_house.add(house_number);
-  }
-
-  for (var house_number = street_length * 2;
-      house_number >= 2;
-      house_number -= 2) {
-    left_house.add(house_number);
-  }
-
-  if (left_house.contains(adress_number)) {
-    return right_house[left_house.indexOf(adress_number)];
-  } else {
-    return left_house[right_house.indexOf(adress_number)];
-  }
+  final index = (adress_number - 1) ~/ 2;
+  return (street_length * 2) - (2 * index);
 }
