066544f2d0ff6b9c66836d05bae2794094ace908
Thaís <thais.calixto@mail.uft.edu.br>
2021-07-05 11:27:33 -0300

    dumb overTheRoad
    Dei push nesse código novamente para mostrar a tela do VScode sem aparecer que o código foi modificado

--- lib/road.dart
+++ lib/road.dart
@@ -1,2 +1,22 @@
-int overTheRoad(int adress_number, int street_length) =>
-    street_length * 2 - adress_number + 1;
+int overTheRoad(int adress_number, int street_length) {
+  var right_house = <int>[];
+  var left_house = <int>[];
+
+  for (var house_number = 1;
+      house_number < street_length * 2;
+      house_number += 2) {
+    right_house.add(house_number);
+  }
+
+  for (var house_number = street_length * 2;
+      house_number >= 2;
+      house_number -= 2) {
+    left_house.add(house_number);
+  }
+
+  if (left_house.contains(adress_number)) {
+    return right_house[left_house.indexOf(adress_number)];
+  } else {
+    return left_house[right_house.indexOf(adress_number)];
+  }
+}
